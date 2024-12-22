13d9be6b16f93bd25860a850c035bac6705cacbf
Mahigavale <Maheshgavale07@gmail.com>
2024-12-22 12:11:07 +0530

    "use state"

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import './App.css';
 
  import {HomeComponent} from './Components/HomeComponent';
  import DemoHook from './Components/DemoHook';
+ import DemoUseState from './Components/DemoUseState';
 
 function App() {
  
 
-   let Profile={
-      name:"mahesh",
-      email:"maheshgavale07@gmail.com",
-      city:"Pune",
-      Pincode:413606
-   }
+   // let Profile={
+   //    name:"mahesh",
+   //    email:"maheshgavale07@gmail.com",
+   //    city:"Pune",
+   //    Pincode:413606
+   // }
 
    return(<>
-   <HomeComponent value={Profile}></HomeComponent>
-   <DemoHook></DemoHook>
+   {/* <HomeComponent value={Profile}></HomeComponent>
+   <DemoHook></DemoHook> */}
+<DemoUseState></DemoUseState>
 
    </>);
 }
---
+++ src/Components/DemoUseState.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+
+function DemoUseState()
+{
+
+
+    let [count, setcount]=useState(0);
+
+    return(<>
+    <div style={{textAlign:"center" , marginTop:"30px"}}>
+    <button  onClick={()=>(setcount(count+1))}>Increament</button>
+    <span> </span>
+    <button onClick={()=>(setcount(count=0))}>Reset</button>
+    <span> </span>
+    <button onClick={()=>(setcount(count-1))}>Decreament</button>
+    <h1>{count}</h1>
+    </div>
+    </>);
+
+
+
+}
+
+export default DemoUseState;
